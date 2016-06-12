0f348150c99c9722545161ce39ae2551ed63b313
caiw <525311175@qq.com>
2016-06-12 10:32:56 +0800

    various task template

--- app/src/ui/page_task/popup_task.js
+++ app/src/ui/page_task/popup_task.js
@@ -64,6 +64,87 @@ var AttachmentList = React.createClass({
     }
 });
 
+var TaskTemplate = React.createClass({
+    getDom1: function(){
+        return (
+            <div className='taskTemplateContainer'>
+                <div className='line name'>
+                    <label>Root Cause</label>
+                    <textarea defaultValue={''}></textarea>
+                </div>
+                
+                <div className='line name'>
+                    <label>Solution</label>
+                    <textarea defaultValue={''}></textarea>
+                </div>
+
+                <div className='line name'>
+                    <label>执行</label>
+                    <textarea defaultValue={''}></textarea>
+                </div>
+
+                <div className='line name'>
+                    <label>反馈</label>
+                    <textarea defaultValue={''}></textarea>
+                </div>
+                <div className='line name'>
+                    <button>导出ppt</button>
+                </div>
+                
+            </div>
+        )
+    },
+
+    getDom2: function(){
+        return (
+            <div className='taskTemplateContainer'>
+                <div className='line'>
+                    <button>导入excel</button>
+                </div>
+                <img src='/app/res/curve.png'/>
+            </div>
+        )
+    },
+
+    updateJqueryComponent: function(){
+        (function updateTaskTemplateDropdown(){
+            var container = this.refs.templateTypeDropdown;
+            var options = [{
+                id: 0,
+                label: "Hot Issue 模版"
+            },{
+                id: 1,
+                label: "尺寸检测模版"
+            }];
+
+            var defaultKey = 0;
+            var param = {
+                id: "templateTypeDropdown", //string.
+                defaultKey: defaultKey, //string. existed id in options.
+                options: options,
+                onchange: (function(key){
+                    //this.refs.taskTemplatePanel.updateByType(key);
+                }).bind(this),
+            };
+            this.templateTypeDropdown = CDropDown.create(container, param);
+        }).call(this);
+    },
+
+    componentDidMount: function(){
+        this.updateJqueryComponent();
+    },
+
+    render: function(){
+        return (
+            <div className='taskTemplateContainer'>
+                <span ref='templateTypeDropdown'/>
+                {this.getDom2()}
+            </div>
+        )
+        
+    }
+
+});
 
 var TaskPopup = React.createClass({
 	getInitialState: function() {
@@ -162,7 +243,7 @@ var TaskPopup = React.createClass({
 
                 <div className="line taskType">
                     <label>任务模版</label>
-                    <span ref='taskTemplateDropdown'/>
+                    <TaskTemplate/>
                 </div>
 	        </div>
 	    );   
@@ -193,31 +274,6 @@ var TaskPopup = React.createClass({
 	        };
 	        this.priorityDropdown = CDropDown.create(container, param);
     	}).call(this);
-
-
-
-
-    	(function updateTaskTemplateDropdown(){
-    		var container = this.refs.taskTemplateDropdown;
-	        var options = [{
-	            id: 0,
-	            label: "Hot Issue 模版"
-	        },{
-	            id: 1,
-	            label: "尺寸检测模版"
-	        }];
-
-	        var defaultKey = this.state.taskTemplateType;
-	        var param = {
-	            id: "taskTemplateDropdown", //string.
-	            defaultKey: defaultKey, //string. existed id in options.
-	            options: options,
-	            onchange: (function(key){
-	            	//this.refs.taskTemplatePanel.updateByType(key);
-	            }).bind(this),
-	        };
-	        this.taskTemplateDropdown = CDropDown.create(container, param);
-    	}).call(this);
     },
 
     onOkClk:function() {
@@ -241,7 +297,7 @@ var TaskPopup = React.createClass({
     },
     render: function() {
         var content = this.getContent();
-        return (<MessageBox width={700} title={this.state.popupTitle} okHandler={this.onOkClk} ref='msgbox' children={content}/>);
+        return (<MessageBox width={700} title={'添加豆豆'} okHandler={this.onOkClk} ref='msgbox' children={content}/>);
     },
 });
 
