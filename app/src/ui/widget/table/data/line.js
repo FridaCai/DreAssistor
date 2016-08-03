325b81e26016d84210c7cbea1585ab4868bb2012
caiw <525311175@qq.com>
2016-08-03 16:56:50 +0800

    refactor;

--- app/src/ui/widget/table/data/line.js
+++ app/src/ui/widget/table/data/line.js
@@ -12,23 +12,8 @@ class Line {
 	init(param){
 		this.id = param.id || Util.generateUUID();
 		this.cells = param.cells;
-        this._dom = this._createDom(); 
-
-
-        this.signal_expand_toggle = new Signal();
-
-        this.cells.map((function(cell){
-        	if(cell instanceof ExpandCell){
-        		cell.signal_expand_toggle.listen((function(){
-	        		this.signal_expand_toggle.dispatch();
-	        	}).bind(this));	
-        	}
-        }).bind(this));
 	}
 	
-	getCellAt(i){
-		return this.cells[i];
-	}
 	constructor(){
 	   this.cells = [];
 	}
