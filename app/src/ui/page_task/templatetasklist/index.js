6399eba10c48cc2eeeb2d97751a067e01d0292b3
Lenovo <Lenovo>
2016-10-11 14:55:27 +0800

    fix drag issue in ie: add 'href='javascript:void(0);''

--- app/src/ui/page_task/templatetasklist/index.js
+++ app/src/ui/page_task/templatetasklist/index.js
@@ -5,6 +5,7 @@ import Request from '../../../request.js';
 import Util from 'Util';
 import MessageBox from 'MessageBox';
 import TemplateTasks from '../../../config/get_template_tasks';
+import './style.less';
 
 var TemplateTaskList = React.createClass({
 	getInitialState: function() {
@@ -31,11 +32,30 @@ var TemplateTaskList = React.createClass({
         
     },
 
+
+
+    // to get IE to work
+    /*onDragEnter: function (e) {
+        console.log('onDragEnter');
+        return false;
+    },*/
+
+
+
+
     onDragOver: function(e){
-      e.preventDefault(); //otherwise, ondrop does not work.
+        e.preventDefault(); // allows us to drop
+        //console.log('onDragOver');
+        //return;
+
+        
+       // this.className = 'over';
+       // e.dataTransfer.dropEffect = 'copy';
+        //return false;
     },
     onDragStart: function(task, e){
-        e.dataTransfer.setData("text", task.template.type);
+        e.dataTransfer.setData("text", task.template.type.toString());
+        //e.dataTransfer.effectAllowed = "move";
     },
    
     render: function() {
@@ -50,32 +70,29 @@ var TemplateTaskList = React.createClass({
             return {id: task.id, label: task.label}
         });
         API.setTemplateEnum(templateenum);
-
-
-
-
         //<MessageBox ref='msgbox' msg='想添加新的豆豆模版？请将需求描述提交给开发人员：525311175@qq.com.'/>
         return (
             <div className='taskList'>
                 
 				<span className="label label-primary addTaskBtn" onClick={this.onAddTaskBtnClk}>+</span>
-				{
-					API.getTemplateTasks().map((function(task){
-						var id = task.id;
-						var label = task.label;
-						var style = {
-							backgroundColor: Util.convertIntColorToHex(task.markColor),
-						}
-						return (
-							<a draggable='true' className='task' style={style} key={id}
-									onClick={this.onEditTaskClk.bind(this, task)}
-                                        onDragOver={this.onDragOver}
-                                        onDragStart={this.onDragStart.bind(this, task)}>
-								{label}
-							</a>
-						)
-					}).bind(this))
-				}
+                    {
+                        API.getTemplateTasks().map((function(task){
+                            var id = task.id;
+                            var label = task.label;
+                            var style = {
+                                backgroundColor: Util.convertIntColorToHex(task.markColor),
+                            }
+                            return (
+                                <a href='javascript:void(0);' draggable='true' className='task' style={style} key={id}
+                                            onDragOver={this.onDragOver}
+                                            onDragEnter={this.onDragEnter}
+                                            onDragStart={this.onDragStart.bind(this, task)}
+                                            onClick={this.onEditTaskClk.bind(this, task)}>
+                                    {label}
+                                </a>
+                            )
+                        }).bind(this))
+                    }
             </div>
         );    
     }
