739c93d6f3a4c662708b72353f2c5397a380b89f
caiw <525311175@qq.com>
2016-10-11 16:51:22 +0800

    add config for different env;

---
+++ app/src/config/system_alpha.js
@@ -0,0 +1,6 @@
+/*connect from mac to lenovo*/
+var SystemConfig = {
+	backendAPI: 'http://192.168.1.107:8080/v1/'
+}
+
+module.exports = SystemConfig;
---
+++ app/src/config/system_prod.js
@@ -0,0 +1,5 @@
+var SystemConfig = {
+	backendAPI: 'http://PCHDW00039.patac.shanghaigm.com:8080/v1/'
+}
+
+module.exports = SystemConfig;
