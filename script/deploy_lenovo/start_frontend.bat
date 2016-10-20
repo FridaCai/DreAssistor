bcb9da4b25e454dde5db29b42c16a06ce9dc8980
Liu Pai 刘湃(CSpp8e3PTI <spp8e3@patac.shanghaigm.com>
2016-10-20 10:16:36 +0800

    fix script

--- script/deploy_lenovo/start_frontend.bat
+++ script/deploy_lenovo/start_frontend.bat
@@ -6,6 +6,6 @@ echo start frontend...
 
 cd D:\FridaStation\DreAssistor
 git pull origin master
-cp  app/src/config/system_lenovo.js app/src/config/system.js
+copy  app\src\config\system_patac_server.js app\src\config\system.js
 npm run deploy-lenovo
 
--- script/deploy_patac_server/start_frontend.bat
+++ script/deploy_patac_server/start_frontend.bat
@@ -6,5 +6,5 @@ echo start frontend...
 
 cd D:\DreAssisstor\site\DreAssistor
 git pull origin master
-cp  app/src/config/system_patac_server.js app/src/config/system.js
+copy  app\src\config\system_patac_server.js app\src\config\system.js
 npm run deploy-patac-server
