11b2557de77dccca0a829deb63cc3638beb65c93
Liu Pai 刘湃(CSpp8e3PTI <spp8e3@patac.shanghaigm.com>
2016-10-20 16:30:04 +0800

    attachment

--- app/src/ui/widget/attachmentlist/index.js
+++ app/src/ui/widget/attachmentlist/index.js
@@ -1,6 +1,7 @@
 import Util from 'Util';
 import Attachment from '../../page_task/data/attachment.js';
 import "./style.less";
+import Request from 'Request';
 
 var AttachmentList = React.createClass({
 	getInitialState: function(){
@@ -16,25 +17,91 @@ var AttachmentList = React.createClass({
 	},
 	fileElemChange: function(e){
 	    e.preventDefault();
-	    //call service to upload file.
+	    
 	    var files = e.target.files;
+	    const maxFileSize = 2000000; //2m
+
+	    var formData = new FormData();
 
 	    for(var i=0; i<files.length; i++){
 	      var file = files[i];
-	      var label = file.name;
-	      var url = '/app/res/' + label;
-	      var attachment = Attachment.create({
+	      if(file.size < maxFileSize){
+	      	formData.append('uploads[]', file, file.name);	
+	      }else{
+	      	console.log('file is too large');
+	      }
+    	}
+
+
+
+
+
+    	var url = Request.getBackendAPI('uploadfile');
+	    $.ajax({
+	      url: url,
+	      type: 'POST',
+	      data: formData,
+	      processData: false,
+	      contentType: false,
+	      success: function(data){
+	          console.log('upload successful!\n' + data);
+	      },
+	      xhr: function() {
+	        // create an XMLHttpRequest
+	        var xhr = new XMLHttpRequest();
+
+	        // listen to the 'progress' event
+	        xhr.upload.addEventListener('progress', function(evt) {
+
+	          if (evt.lengthComputable) {
+	            // calculate the percentage of upload completed
+	            var percentComplete = evt.loaded / evt.total;
+	            percentComplete = parseInt(percentComplete * 100);
+
+	            // update the Bootstrap progress bar with the new percentage
+	            //$('.progress-bar').text(percentComplete + '%');
+	            //$('.progress-bar').width(percentComplete + '%');
+	            console.log(percentComplete)
+
+	            // once the upload reaches 100%, set the progress bar text to done
+	            //if (percentComplete === 100) {
+	              //$('.progress-bar').html('Done');
+	            //}
+
+	          }
+
+	        }, false);
+
+	        return xhr;
+	      }
+	    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	      /*var attachment = Attachment.create({
 	        id: Util.generateUUID(),
 	        label: label,
 	        url: url,
 	      })
-	      this.state.attachments.add(attachment);
-	    }
+	      this.state.attachments.add(attachment);*/
 
-	    if(this.props.onAdd){
+	    /*if(this.props.onAdd){
 			this.props.onAdd.call(this.props.scope, this.state.attachments);
 		}
-	    this.forceUpdate();
+	    this.forceUpdate();*/
 	},
 
 	onDelete: function(id){
@@ -96,7 +163,7 @@ var AttachmentList = React.createClass({
 
 
 		          <form className='upload'>
-		            <input type="file" ref="fileElem" multiple accept="*/*" onChange={this.fileElemChange}/>
+		            <input name='uploadfile' type="file" ref="fileElem" multiple accept="*/*" onChange={this.fileElemChange}/>
 		          </form>
 		        </div>
 			</div>
